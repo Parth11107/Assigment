c8171d94cb4a4db617f4048ecc524851111540a1
Parth11107 <parthchashmawala111@gmail.com>
2025-10-08 15:21:12 +0530

    as1.c

--- as1.c
+++ as1.c
@@ -4,11 +4,12 @@ void main()
  {
    int arr[] = {10, 4, 8, 22, 15};
    int p,s;
-    int n = sizeof(arr) / sizeof(arr[0]);
+  int i;
+   int n = sizeof(arr) / sizeof(arr[0]);
 
     int first = p, second = s;
 
-    for (int i = 0; i < n; i++) {
+    for ( i = 0; i < n; i++) {
         if (arr[i] > first) {
             second = first;
             first = arr[i];
